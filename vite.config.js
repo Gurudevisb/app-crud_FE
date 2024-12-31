1d170b4c7f883001fc8bf7b72c1d112d62c127ea
Gurudevisb <bullagurudevi.com>
2024-12-31 23:01:29 +0530

    all files added

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+
+    server: {
+        host: '0.0.0.0',   // Bind to all interfaces
+        port: 5173,         // Ensure the port is correctly set
+      },
+
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
