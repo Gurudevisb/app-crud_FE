e991a0e90b3f0eb69fcd59bd66048a138e98892e
Gurudevisb <bullagurudevi.com>
2025-01-07 08:20:39 +0530

    all files added

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base:'/Frontend/',
+  base: '/',
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
